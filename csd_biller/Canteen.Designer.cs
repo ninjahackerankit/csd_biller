befc0162a6fcef99cb93e331325d00a7f8803f16
ankitkumar90313@gmail.com <ankitkumar90313@gmail.com>
2020-09-14 21:07:45 +0530

    csd biller  14/9/2020

---
+++ csd_biller/Canteen.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace csd_biller
+{
+    partial class Canteen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Canteen));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txtRate = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.txttotalprice = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTotalBill = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Item";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(429, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Price";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(610, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Quantity";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(763, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Total";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(2, 106);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(343, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // txtRate
+            // 
+            this.txtRate.Location = new System.Drawing.Point(413, 107);
+            this.txtRate.Name = "txtRate";
+            this.txtRate.Size = new System.Drawing.Size(100, 20);
+            this.txtRate.TabIndex = 5;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(595, 107);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(100, 20);
+            this.txtQuantity.TabIndex = 6;
+            this.txtQuantity.TextChanged += new System.EventHandler(this.txtQuantity_TextChanged);
+            // 
+            // txttotalprice
+            // 
+            this.txttotalprice.Location = new System.Drawing.Point(743, 106);
+            this.txttotalprice.Name = "txttotalprice";
+            this.txttotalprice.Size = new System.Drawing.Size(100, 20);
+            this.txttotalprice.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(309, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Add To Bill";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 226);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(631, 188);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(309, 447);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Print";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(754, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Total Bill";
+            // 
+            // txtTotalBill
+            // 
+            this.txtTotalBill.Location = new System.Drawing.Point(727, 244);
+            this.txtTotalBill.Name = "txtTotalBill";
+            this.txtTotalBill.Size = new System.Drawing.Size(100, 20);
+            this.txtTotalBill.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(814, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Close";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Canteen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 506);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtTotalBill);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txttotalprice);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtRate);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Canteen";
+            this.Text = "Canteen";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtRate;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.TextBox txttotalprice;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTotalBill;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ csd_biller/Canteen.cs
@@ -0,0 +1,298 @@
+﻿using PrinterUtility;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ESC_POS_USB_NET.Printer;
+using ESC_POS_USB_NET.Enums;
+
+namespace csd_biller
+{
+    public partial class Canteen : Form
+    {
+        public static DataTable dt;
+        public static DataTable bill;
+        public static int count;
+        public Canteen()
+        {
+            InitializeComponent();
+
+            bill = new DataTable();
+            bill.Columns.Add("Name");
+            bill.Columns.Add("Price");
+            bill.Columns.Add("Quantity");
+            bill.Columns.Add("Total");
+
+            dt = new DataTable();
+            // create table which have article and price
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Price");
+
+            dt = Read();
+
+            List<string> l = new List<string>();
+            foreach(DataRow row in dt.Rows)
+            {
+                l.Add(row["Name"].ToString());
+            }
+            comboBox1.DataSource = l;
+
+            //comboBox1.DataSource = Articles();
+            comboBox1.SelectedIndex = -1;
+            Read();
+
+            count = 1;
+        }
+
+        /// <summary>
+        /// READ THE PRICE FILE
+        /// </summary>
+        /// <returns></returns>
+        public static DataTable Read()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\project\csd_biller\csd_main.txt");
+            
+            //create table with data
+            DataTable dt1 = new DataTable();
+            dt1.Columns.Add("Name");
+            dt1.Columns.Add("Price");
+            foreach (string line in lines)
+            {
+                string[] ar = line.Split('|');
+                object[] o1 = { ar[0].ToString(), Convert.ToDecimal(ar[1].ToString()) };
+                dt1.Rows.Add(o1);
+
+            }
+            return dt1;
+        }
+
+        /// <summary>
+        /// return articles names for combo box list, but now not needed
+        /// </summary>
+        /// <returns></returns>
+        public List<string> Articles()
+        {
+            List<string> articles = new List<string>();
+            articles.Add("Lux Toilet Soap    ");
+            articles.Add("Clinic Plus Shampoo");
+
+            return articles;
+        }
+
+        /// <summary>
+        /// WHEN ARTICLE IS SELECTED FROM DROP DOWN, THEN AUTOMATICALLY FILL THE UNIT PRICE OF ITEM 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string article = comboBox1.SelectedItem == null ? "" : comboBox1.SelectedItem.ToString();
+            string price = "";
+            foreach (DataRow dr in dt.Rows)
+            {
+                if (dr[0].ToString() == article)
+                {
+                    price = dr[1].ToString();
+                    continue;
+                }
+            }
+            txtRate.Text = price;
+            txtQuantity.Text = "";
+            txttotalprice.Text = "";
+        }
+
+        /// <summary>
+        /// WHEN WUANTITY OF ITEM IS ENTERED, AUTOMATICALLY CALCULATE TOTAL PRICE
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtQuantity_TextChanged(object sender, EventArgs e)
+        {
+            int qty = txtQuantity.Text == "" ? 0 : Convert.ToInt32(txtQuantity.Text.ToString());
+            //txtprice = qty *
+
+            // Presuming the DataTable has a column named Date.
+            string article = comboBox1.SelectedItem.ToString();
+            string price = txtRate.Text.ToString();
+
+            txttotalprice.Text = (qty * (Convert.ToDecimal(price))).ToString();
+        }
+
+        /// <summary>
+        /// ADD ITEM TO BILL
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtQuantity.Text.ToString() == "")
+            {
+                MessageBox.Show("Pls Enter Quantity of Item !!");
+            }
+            else
+            {
+                string article = comboBox1.SelectedItem.ToString();
+                object[] o = { article, txtRate.Text.ToString(), txtQuantity.Text.ToString(), txttotalprice.Text.ToString() };
+                bill.Rows.Add(o);
+                dataGridView1.DataSource = bill;
+                dataGridView1.AutoResizeColumns();
+
+                decimal ttl = 0;
+                foreach(DataRow dr in bill.Rows)
+                {
+                    ttl = ttl + Convert.ToDecimal(dr["Total"]);
+                }
+
+                txtTotalBill.Text = ttl.ToString();
+            }
+        }
+
+        /// <summary>
+        /// PRINT THE BILL
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DateTime localDate = DateTime.Now;
+            // print bill
+            System.Text.StringBuilder b = new System.Text.StringBuilder();
+            System.Text.StringBuilder f = new System.Text.StringBuilder();
+            double total = 0;
+            foreach (System.Data.DataRow r in bill.Rows)
+            {
+                System.Text.StringBuilder temp = new StringBuilder();
+                System.Text.StringBuilder temp2 = new StringBuilder();
+
+                if (r["Name"].ToString().Length>22)
+                {
+                    temp.Append(r["Name"].ToString().Substring(0, 22) + "    ");
+                    temp.Append(r["Quantity"].ToString() + "    ");
+                    temp.Append(r["Price"].ToString() + "    ");
+                    temp.Append(r["Total"].ToString() + "    ");
+
+                    while(temp.ToString().Length<=48)
+                    {
+                        temp.Append(" ");
+                    }
+
+                    // so rest of string comes below and not in same line (CHECK BELOW COMMENTS)
+                    /// VLCC SOME PRODUCT NAME  2 190.5  281
+                    /// REMAINING NAME
+                    temp.Append(r["Name"].ToString().Substring(22) + "    ");
+
+                    // to save the data in file
+                    temp2.Append(r["Name"].ToString() + "    ");
+                    temp2.Append(r["Quantity"].ToString() + "    ");
+                    temp2.Append(r["Price"].ToString() + "    ");
+                    temp2.Append(r["Total"].ToString() + "    ");
+                }
+                else
+                {
+                    temp.Append(r["Name"].ToString() + "    ");
+                    temp.Append(r["Quantity"].ToString() + "    ");
+                    temp.Append(r["Price"].ToString() + "    ");
+                    temp.Append(r["Total"].ToString() + "    ");
+
+                    // save data in file
+                    temp2.Append(r["Name"].ToString() + "    ");
+                    temp2.Append(r["Quantity"].ToString() + "    ");
+                    temp2.Append(r["Price"].ToString() + "    ");
+                    temp2.Append(r["Total"].ToString() + "    ");
+                }
+                
+                
+                //}
+                total = total + Convert.ToDouble(r["Total"].ToString());
+                b.Append(temp.ToString());
+                b.Append("\n");
+
+                f.Append(temp2.ToString());
+                f.Append("\n");
+            }
+
+            //rounding off the decimal number
+            int totalbill = RoundOff(total);
+            string totalstr = " Total bill = " + totalbill.ToString();
+
+            /// bill printer start
+            Printer printer = new Printer("EPSON TM-T82II Receipt5");
+            printer.Separator('*');
+            printer.AlignCenter();
+            printer.BoldMode("SAINIK CANTEEN ENTERPRISES");
+            printer.Append("CP COLONY, ROAD NO 1, GAYA, BIHAR 823001");
+            printer.Separator();
+            printer.AlignLeft();
+            printer.Font("Name                    Quantity Price Total", Fonts.FontA);
+            printer.Separator();
+            //printer.Font("Font A", Fonts.FontA);
+            printer.Font(b.ToString(), Fonts.FontA);
+
+            printer.Separator();
+            printer.InitializePrint();
+            printer.AlignRight();
+            printer.NewLines(1);
+            printer.Font(totalstr, Fonts.FontA);
+            printer.NewLines(1);
+            printer.Separator();
+            printer.AlignCenter();
+            printer.Append("Thank You!!! ");
+            printer.Separator('*');
+
+            printer.FullPaperCut();
+            printer.PrintDocument();
+            /// bill printer end
+
+
+            bill.Clear();
+            dataGridView1.DataSource = null;
+            txtTotalBill.Text = "";
+            txttotalprice.Text = "";
+
+            DateTime today = DateTime.Today;
+            string time = DateTime.Now.ToString("h:mm:sec");
+            string[] tar = time.Split(':');
+            string cur_time = tar[0].ToString() + "_" + tar[1].ToString() + "_" + tar[2].ToString() + "_";
+            string path = @"C:\project\bills\bill_" + today.ToString("dd-MM-yyyy") + "_"  + cur_time+ "_" +count.ToString() + ".txt";
+            count = count + 1;
+            //save the data in text file.
+            System.IO.File.WriteAllText(path, f.ToString());
+
+        }
+
+        /// <summary>
+        /// close button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public int RoundOff(double num)
+        {
+            double roundnum = num; 
+            for(int i=0; i< num.ToString().IndexOf('.'); i++)
+            {
+                roundnum = roundnum / 10;
+            }
+
+            if(roundnum>0)
+            {
+                int n = Convert.ToInt32(num.ToString().Substring( 0, Convert.ToInt32(num.ToString().IndexOf('.')) ) )+1;
+                return n;
+            }
+            else
+            {
+                return Convert.ToInt32(num);
+            }
+            
+        }
+    }
+}
